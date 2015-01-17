2ec58c8c6fd24c95e5e5bd1476ef9f98017c1f41
atsushi-kambayashi <atsushi.kambayashi@gree.net>
2015-01-18 00:57:24 +0900

    added sdwebimage

---
+++ nhkfind/Bridge-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridge-Header.h
+//  nhkfind
+//
+//  Created by atsushi.kambayashi on 2015/01/18.
+//  Copyright (c) 2015年 atsushi.kambayashi. All rights reserved.
+//
+
+#ifndef nhkfind_Bridge_Header_h
+#define nhkfind_Bridge_Header_h
+
+#import <UIImageView+WebCache.h>
+
+#endif
