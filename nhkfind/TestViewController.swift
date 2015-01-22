7c3b3ff3b23b74de791b5a1745f9f86678a5edc0
atsushi-kambayashi <atsushi.kambayashi@gree.net>
2015-01-22 23:02:30 +0900

    added scroll sample view

---
+++ nhkfind/TestViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TestViewController.swift
+//  nhkfind
+//
+//  Created by atsushi.kambayashi on 2015/01/22.
+//  Copyright (c) 2015年 atsushi.kambayashi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TestViewController:UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+}
