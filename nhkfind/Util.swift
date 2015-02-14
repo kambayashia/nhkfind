fb7eb1fa3f061af019a23679a541e10851624710
atsushi-kambayashi <atsushi.kambayashi@gree.net>
2015-02-14 22:15:09 +0900

    added placeholder image

---
+++ nhkfind/Util.swift
@@ -0,0 +1,16 @@
+//
+//  Util.swift
+//  nhkfind
+//
+//  Created by atsushi.kambayashi on 2015/02/14.
+//  Copyright (c) 2015年 atsushi.kambayashi. All rights reserved.
+//
+
+import Foundation
+
+class Util
+ {
+  class func placeholderImage() -> UIImage {
+    return UIImage(named: "nowloading")!
+  }
+}
